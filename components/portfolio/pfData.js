5e56d8fc606fbbe681a158072b21a36a61dbaffd
Sander Byrkjeland <sunbyr@gmail.com>
2021-06-07 02:44:32 +0200

    added calculator to pf

--- components/portfolio/pfData.js
+++ components/portfolio/pfData.js
@@ -4,12 +4,6 @@ export const pfData = [
 		slug: "spaceX",
 		image:
 			"https://res.cloudinary.com/dfhcrdprn/image/upload/v1622830690/portfolio/spaceX_fhwxhv.png",
-		image2:
-			"https://res.cloudinary.com/dfhcrdprn/image/upload/v1623019311/portfolio/spacex2_h29vb4.png",
-		image3:
-			"https://res.cloudinary.com/dfhcrdprn/image/upload/v1623019312/portfolio/spacex3_jskfnr.png",
-		image4:
-			"https://res.cloudinary.com/dfhcrdprn/image/upload/v1623020401/portfolio/spacex4_ukeuev.png",
 		title: "SpaceX Microsite",
 		languages: "React | Sass",
 		blurb:
@@ -27,11 +21,27 @@ export const pfData = [
 			"https://res.cloudinary.com/dfhcrdprn/image/upload/v1622999689/portfolio/pkmn_cxmykk.png",
 		title: "Pokédex project",
 		languages: "Next.js | Tailwind",
-		slug: "A little project displaying all pokemon, to practice working with getServerSideProps and getStaticProps in Next.js",
+		blurb:
+			"A little project displaying all pokemon, to practice working with getServerSideProps and getStaticProps in Next.js.",
 		link: "https://next-pokedex-sander.vercel.app/",
 		goal: "Build a site which showcases some kind of information fetched from an API.",
 		notes:
 			"This project was mainly built to practice my knowledge of Next.JS, as well as Tailwind. I plan on developing it further, using it as a little side project to keep building up my skills, as well as deepening my knowledge of how Next.JS works",
 		github: "https://github.com/Sander-TB/next-pokedex",
+	},
+	{
+		id: 3,
+		slug: "calculator",
+		image:
+			"https://res.cloudinary.com/dfhcrdprn/image/upload/v1623026118/portfolio/calc_wxwhsq.png",
+		title: "Calculator",
+		languages: "Vanilla JS | HTML | CSS",
+		blurb: "A simple calculator in the web browser.",
+		link: "https://sander-js-calculator.netlify.app/",
+		goal: "Build a small project showing your grasp of key Javascript concepts",
+		notes:
+			"This was an early project of mine. The calculator is fully functional and styled with regular css. It's a little simple example of what is possible to make with to make with only javascript",
+		github:
+			"https://github.com/Sander-TB/Practice-Projects/tree/master/Calculator",
 	},
 ];
