eeb5ea44df4f6010ec6f84aa0d8b11485004439b
Sander Byrkjeland <sunbyr@gmail.com>
2021-06-07 02:12:05 +0200

    small hp fix

--- components/home/Home.tsx
+++ components/home/Home.tsx
@@ -4,7 +4,7 @@ import BgAnimation from "../animations/BgAnimation";
 export default function Home() {
 
   return (
-    <div className="flex flex-col ml-5 md:ml-20 lg:ml-0 h-95vh" id="home">
+    <div className="flex flex-col ml-2 md:ml-20 lg:ml-0 h-95vh" id="home">
       <h1 className="text-green text-5xl md:text-7xl lg:text-9xl mt-32 md:mt-48 font-bold">.me(<span className="text-white">&#123;<span className="italic">Sander</span>&#125;</span>)</h1>
       <p className="text-white text-xl md:text-3xl pl-8 md:pl-10 lg:pl-20 mt-3 md:mt-7">Front-End Developer,</p>
       <p className="text-green text-xl md:text-3xl pl-8 md:pl-10 lg:pl-20">Tech-Lover & Nerd</p>
